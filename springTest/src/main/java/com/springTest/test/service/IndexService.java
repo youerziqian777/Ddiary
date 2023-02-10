a930f25b804eb8ea681c522ad2498adfb7aa3cd1
mixTheMilk <1158775391@qq.com>
2023-02-10 17:50:04 +0800

    新增了SSM项目，原spring项目新增了简单的逻辑，用于区分构造注入和set注入

--- springTest/src/main/java/com/springTest/test/service/IndexService.java
+++ springTest/src/main/java/com/springTest/test/service/IndexService.java
@@ -1,16 +1,27 @@
 package com.springTest.test.service;
 
+import com.alibaba.fastjson.JSON;
+import com.springTest.test.dao.UserDao1;
+import com.springTest.test.dao.UserDao2;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-/**
- * @ClassNameIndexService
- * @Description
- * @Author
- * @Date2022/11/7 17:15
- **/
+import java.util.HashMap;
+import java.util.Map;
+
 @Service
 public class IndexService {
+    @Autowired
+    UserDao1 userDao1;
+
+    @Autowired
+    UserDao2 userDao2;
     public String test(){
-        return "testtest";
+        Map<String,String> resultMap = new HashMap<>();
+        resultMap.put("set注入",userDao1.getUserName());
+        resultMap.put("构造器注入", userDao2.toString());
+        return JSON.toJSONString(resultMap);
     }
+
+
 }
